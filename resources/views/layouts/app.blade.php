a11b13612fe72dc6fbbb3f93b809d6bb60f24ffe
yama-bun <baseball8296@gmail.com>
2021-03-11 06:53:39 +0900

    商品詳細画面実装

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <title>@yield('title') | {{ config('app.name', 'Laravel') }}</title>
+        <link rel="stylesheet" href="{{ asset('/css/app.css') }}">
+    </head>
+    <body>
+        <nav class="navbar navbar-light bg-light">
+            <div class="container">
+                <a href="/" class="navbar-brand">{{ config('app.name', 'Laravel') }}</a>
+                <a href="#" class="fas fa-shopping-cart"></a>
+            </div>
+        </nav>
+        @yield('content')
+    </body>
+</html>
